bdaa74dad57aa16f759c83150556976cca307afe
HersonCastillo <30848872+HersonCastillo@users.noreply.github.com>
2018-07-26 22:23:04 -0600

    changes +error

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 @Component({
-  selector: 'app-error',
-  templateUrl: './error.component.html',
-  styleUrls: ['./error.component.css']
+    selector: 'app-error',
+    templateUrl: './error.component.html',
+    styleUrls: ['./error.component.css']
 })
 export class ErrorComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-    $("body, html").on('contextmenu', function(){
-			return false;
-		});
-  }
-
+    constructor() { }
+    ngOnInit() {
+        $("body, html").on('contextmenu', function(){
+            return false;
+        });
+        $("title").text("Ups parece que la página no existe");
+    }
 }
